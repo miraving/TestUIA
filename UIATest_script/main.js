1bb164018c3f3846fe04b5cf4dc9610267dfd585
Vitaliy Obertinskiy <vitaliy_obertinskiy@epuavinw0321.kyiv.epam.com>
2014-03-25 12:49:14 +0200

    refcat. log structure

--- UIATest_script/main.js
+++ UIATest_script/main.js
@@ -1,16 +1,17 @@
-UIALogger.logMessage("Start APP");
+UIALogger.logMessage("Initialization");
 var target = UIATarget.localTarget();
 var app = target.frontMostApp();
 var navigationBar = app.navigationBar();
 var window = app.mainWindow();
 var tableView = window.tableViews()[0];
 
-
+UIALogger.logStart("Start: Add new row");
 UIALogger.logMessage("+ tap button");
 var plusButton = navigationBar.rightButton();
 plusButton.tap();
+UIALogger.logPass("End: Add new row");
 
-
+UIALogger.logStart("Start: Remove cell");
 UIALogger.logMessage("Edit tap button");
 var editButton = navigationBar.leftButton();
 editButton.tap();
@@ -23,7 +24,7 @@ deleteButton.tap();
 
 UIALogger.logMessage("Done tap button");
 editButton.tap();
-
+UIALogger.logPass("End: Remove cell");
 
 
 UIALogger.logMessage("End APP");
